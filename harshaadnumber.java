19b5b4285015213a5269f93c1d5ea0e9ccd71f57
Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>
2024-04-21 22:42:06 +0530

    harshaad number solution

---
+++ harshaadnumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int dup = x;
+        while(dup>0){
+            sum = sum + (dup%10);
+            dup /= 10; 
+        }
+        if(x % sum == 0) {return sum;}
+        else {
+
+        return -1;
+        }
+    }
+}
